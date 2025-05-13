ffbfb488d9d68631e4f1c7b1ff3a4420ebbfac80
hnascx <henrique.nascimento12@hotmail.com>
2025-05-13 14:52:16 -0300

    feat: Create fetch recent questions use case

--- src/domain/forum/enterprise/entities/question.ts
+++ src/domain/forum/enterprise/entities/question.ts
@@ -1,7 +1,7 @@
-import dayjs from 'dayjs'
 import { Entity } from '@/core/entities/entity'
 import { UniqueEntityID } from '@/core/entities/unique-entity-id'
 import { Optional } from '@/core/types/optional'
+import dayjs from 'dayjs'
 import { Slug } from './value-objects/slug'
 
 export interface QuestionProps {
@@ -80,7 +80,7 @@ export class Question extends Entity<QuestionProps> {
       {
         ...props,
         slug: props.slug ?? Slug.createFromText(props.title),
-        createdAt: new Date(),
+        createdAt: props.createdAt ?? new Date(),
       },
       id,
     )
