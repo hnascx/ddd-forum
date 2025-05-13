4ceae5c93686f8edc2dd84ceef1b7bf095076c54
hnascx <henrique.nascimento12@hotmail.com>
2025-05-13 13:18:09 -0300

    feat: Create edit answer use case

--- src/domain/forum/application/repositories/answers-repository.ts
+++ src/domain/forum/application/repositories/answers-repository.ts
@@ -1,7 +1,8 @@
 import { Answer } from '../../enterprise/entities/answer'
 
-export interface AnswerRepository {
+export interface AnswersRepository {
   findById(answerId: string): Promise<Answer | null>
   create(answer: Answer): Promise<void>
+  save(answer: Answer): Promise<void>
   delete(answer: Answer): Promise<void>
 }
--- test/repositories/in-memory-answers-repository.ts
+++ test/repositories/in-memory-answers-repository.ts
@@ -1,7 +1,7 @@
-import { AnswerRepository } from '@/domain/forum/application/repositories/answers-repository'
+import { AnswersRepository } from '@/domain/forum/application/repositories/answers-repository'
 import { Answer } from '@/domain/forum/enterprise/entities/answer'
 
-export class InMemoryAnswersRepository implements AnswerRepository {
+export class InMemoryAnswersRepository implements AnswersRepository {
   public items: Answer[] = []
 
   async findById(id: string) {
@@ -16,6 +16,12 @@ export class InMemoryAnswersRepository implements AnswerRepository {
 
   async create(answer: Answer) {
     this.items.push(answer)
+  }
+
+  async save(answer: Answer) {
+    const itemIndex = this.items.findIndex((item) => item.id === answer.id)
+
+    this.items[itemIndex] = answer
   }
 
   async delete(answer: Answer) {
